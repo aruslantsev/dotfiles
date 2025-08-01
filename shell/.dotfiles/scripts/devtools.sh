cd1b8c9887804cee51d6a25d9a6376eb401a092b
Andrei Ruslantsev <andreiruslantsev@gmail.com>
2025-08-01 14:43:24 +0300

    First update

--- shell/.dotfiles/scripts/devtools.sh
+++ shell/.dotfiles/scripts/devtools.sh
@@ -1,15 +1,5 @@
 if [ ${SHOW_CONDA_STATUS:-0} -eq 1 ] && [ -d ~/miniconda3 ]; then
-	__conda_setup="$(~/miniconda3/bin/conda 'shell.bash' 'hook' 2> /dev/null)"
-	if [ $? -eq 0 ]; then
-		eval "$__conda_setup"
-	else
-		if [ -f ~/miniconda3/etc/profile.d/conda.sh ]; then
-			. ~/miniconda3/etc/profile.d/conda.sh
-		else
-			export PATH="~/miniconda3/bin:$PATH"
-		fi
-	fi
-	unset __conda_setup
+	eval "$(~/miniconda3/bin/conda shell.${SHELL##*/} hook)"
 	conda deactivate
 
 	if [ ! -f ~/.condarc ]; then
